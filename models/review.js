fb2ace1d225a3eb0e1ca9d5003da22885a5d3b5a
ah-james <11ahj11@gmail.com>
2021-05-29 17:40:35 -0400

    Adding Review Model

---
+++ models/review.js
@@ -0,0 +1,11 @@
+class Review {
+    constructor(id, userId, album, imageUrl, rating, text) {
+        this.id = id,
+        this.userId = userId,
+        this.album = album,
+        this.artist = artist,
+        this.imageUrl = imageUrl,
+        this.rating = rating,
+        this.text = text
+    }
+}
