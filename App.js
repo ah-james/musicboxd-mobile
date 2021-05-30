07d7772ca4f552561111e5a0f6157b9148795504
ah-james <11ahj11@gmail.com>
2021-05-30 00:22:58 -0400

    Navigation Added

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Navigator from './navigation/Navigator'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Navigator />
-    </View>
-  );
+  return <Navigator />
 }
 
 const styles = StyleSheet.create({
