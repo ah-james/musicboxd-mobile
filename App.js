934d3f0c6f5bc97ce56ce3f2d80acb9b1c0d4090
ah-james <11ahj11@gmail.com>
2021-06-03 23:29:25 -0400

    Adding Redux and Thunk to Application

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import Thunk from 'redux-thunk'
 
 import Navigator from './navigation/Navigator'
+import reviewReducer from './store/reducers/reviewReducer'
+
+// use combineReducers to eventually expand application
+const rootReducer = combineReducers({
+  reviews: reviewReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(Thunk))
 
 export default function App() {
-  return <Navigator />
+  return(
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
